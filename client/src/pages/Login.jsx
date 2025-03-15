2594cd9a65d865b2562f3e9b87154f01facfe8f6
bipulchhetri <bipulchhetri80@gmail.com>
2025-03-16 00:41:53 +0530

    server url

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin= async(e)=>{
     e.preventDefault()
     try{
-     const res= await axios.post("http://localhost:3000/login",{email,password})
+     const res= await axios.post("https://blog-app-x66b.onrender.com/login",{email,password})
       localStorage.setItem('token',res.data.token)
       localStorage.setItem("username",res.data.username)
       navigate('/dashboard')
